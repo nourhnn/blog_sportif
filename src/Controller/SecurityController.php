ad17072ca1aa7d8ac878b6b2b5c350aa8bf874fb
Charly <charlyplanquettepro@gmail.com>
2024-03-02 22:35:15 +0100

    save

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,7 +19,10 @@ class SecurityController extends AbstractController
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        dd($lastUsername);
+        // dd($lastUsername);
+
+        // $userId = $doctrine->getManager()->getRepository(Article::class)->findAll(); 
+        // dd($userId);
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -46,6 +47,7 @@ class ArticleType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('Enregister', SubmitType::class)
         ;
     }
 
