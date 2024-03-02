462b3974529ff43621cb304361431dfb9b125e21
Charly <charlyplanquettepro@gmail.com>
2024-03-02 22:13:31 +0100

    article type form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,6 +17,7 @@ class SecurityController extends AbstractController
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+        dd($lastUsername);
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
